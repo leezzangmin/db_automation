5d511d900dfe9818216fab6055a050d1cc5634f3
leezzangmin <ckdals12345678@gmail.com>
2023-06-23 15:45:56 +0900

    feat: @AllArgsConstructor 추가

--- src/main/java/zzangmin/db_automation/dto/request/RenameColumnRequestDTO.java
+++ src/main/java/zzangmin/db_automation/dto/request/RenameColumnRequestDTO.java
@@ -1,10 +1,12 @@
 package zzangmin.db_automation.dto.request;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
+@AllArgsConstructor
 public class RenameColumnRequestDTO extends DDLRequestDTO {
     private String schemaName;
     private String tableName;
