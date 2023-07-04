60b4d1987036a43bab0a5c4132747b16ebd7df7f
leezzangmin <ckdals12345678@gmail.com>
2023-07-04 15:11:34 +0900

    feat: 변경 이력 추적 엔티티 생성

---
+++ src/main/java/zzangmin/db_automation/entity/ChangeHistory.java
@@ -0,0 +1,27 @@
+package zzangmin.db_automation.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@ToString
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class ChangeHistory {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Enumerated(EnumType.STRING)
+    private CommandType commandType;
+    private String databaseIdentifier;
+    private String schemaName;
+    private String tableName;
+    private String doer;
+    private LocalDateTime doDateTime;
+    private String changeeeeeeeee;
+
+}
