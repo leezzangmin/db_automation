0bc225c34ce3856cff47cc2240baa1c8de49c64e
leezzangmin <ckdals12345678@gmail.com>
2023-07-05 17:27:35 +0900

    feat: 에러 객체로 리턴, 로그 추가

--- src/main/java/zzangmin/db_automation/exception/ExceptionControllerAdvice.java
+++ src/main/java/zzangmin/db_automation/exception/ExceptionControllerAdvice.java
@@ -1,17 +1,27 @@
 package zzangmin.db_automation.exception;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.Arrays;
 
 @Slf4j
 @RestControllerAdvice
 public class ExceptionControllerAdvice {
 
     @ExceptionHandler(Exception.class)
-    public String globalException(Exception e) {
+    public ErrorMessage globalException(Exception e) {
         log.error("Exception Occurred: {}", e.getMessage());
-        e.printStackTrace();
-        return "ERROR. "+ e.getMessage().toString();
+        log.error(Arrays.toString(e.getStackTrace()));
+        return new ErrorMessage("ERROR. "+ e.getMessage().toString());
+    }
+
+    @Getter
+    @AllArgsConstructor
+    static class ErrorMessage {
+        private String errorMessage;
     }
 }
