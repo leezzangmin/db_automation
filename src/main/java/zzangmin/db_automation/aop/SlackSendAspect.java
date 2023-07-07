6c7b7b7fe2242bff203463337b7f36aa5deecd56
leezzangmin <ckdals12345678@gmail.com>
2023-07-07 15:27:58 +0900

    feat: 시작, 종료, 오류 메시지 전송 추가

--- src/main/java/zzangmin/db_automation/aop/SlackSendAspect.java
+++ src/main/java/zzangmin/db_automation/aop/SlackSendAspect.java
@@ -1,22 +1,21 @@
 package zzangmin.db_automation.aop;
 
-import org.aspectj.lang.annotation.AfterReturning;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Pointcut;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.*;
 import org.springframework.stereotype.Component;
 import zzangmin.db_automation.client.SlackClient;
+import zzangmin.db_automation.dto.request.DDLRequestDTO;
 import zzangmin.db_automation.dto.response.DDLResponseDTO;
+import zzangmin.db_automation.info.DatabaseConnectionInfo;
 
+@RequiredArgsConstructor
 @Aspect
 @Component
 public class SlackSendAspect {
 
     private final SlackClient slackClient;
-    @Autowired
-    public SlackSendAspect(SlackClient slackClient) {
-        this.slackClient = slackClient;
-    }
+
 
     @Pointcut("@annotation(org.springframework.web.bind.annotation.PostMapping) " +
             "|| @annotation(org.springframework.web.bind.annotation.PutMapping) " +
@@ -24,9 +23,63 @@ public class SlackSendAspect {
             "|| @annotation(org.springframework.web.bind.annotation.PatchMapping)")
     private void requestMappingMethods() {}
 
+    @Before("requestMappingMethods()")
+    public void sendStartMessageToSlack(JoinPoint joinPoint) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("<DDL Execution Start!>\n");
+        Object[] args = joinPoint.getArgs();
+        for (Object arg : args) {
+            if (arg instanceof DDLRequestDTO) {
+                arg = (DDLRequestDTO) arg;
+                sb.append("CommandType: ");
+                sb.append(((DDLRequestDTO) arg).getCommandType());
+            } else if (arg instanceof DatabaseConnectionInfo) {
+                arg = (DatabaseConnectionInfo) arg;
+                sb.append("connection info:");
+                sb.append(((DatabaseConnectionInfo) arg).databaseSummary());
+            }
+        }
+        slackClient.sendMessage(sb.toString());
+    }
+
     @AfterReturning(pointcut = "requestMappingMethods()", returning = "dto")
-    public void afterReturningSendDTO(Object dto) {
-       slackClient.sendMessage(((DDLResponseDTO) dto).toString());
+    public void afterReturningSendDTO(JoinPoint joinPoint, Object dto) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("<DDL Execution Finished!>\n");
+        Object[] args = joinPoint.getArgs();
+        for (Object arg : args) {
+            if (arg instanceof DDLRequestDTO) {
+                sb.append("CommandType: ");
+                sb.append(((DDLRequestDTO) arg).getCommandType());
+            } else if (arg instanceof DatabaseConnectionInfo) {
+                arg = (DatabaseConnectionInfo) arg;
+                sb.append("connection info:");
+                sb.append(((DatabaseConnectionInfo) arg).databaseSummary());
+            }
+        }
+        sb.append(((DDLResponseDTO) dto).toString());
+        slackClient.sendMessage(sb.toString());
+    }
+
+    @AfterThrowing(pointcut = "requestMappingMethods()", throwing = "error")
+    public void sendErrorMessageToSlack(JoinPoint joinPoint, Throwable error) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("<DDL Execution Failed!>\n");
+        Object[] args = joinPoint.getArgs();
+        for (Object arg : args) {
+            if (arg instanceof DDLRequestDTO) {
+                sb.append("CommandType: ");
+                sb.append(((DDLRequestDTO) arg).getCommandType());
+            } else if (arg instanceof DatabaseConnectionInfo) {
+                arg = (DatabaseConnectionInfo) arg;
+                sb.append("connection info:");
+                sb.append(((DatabaseConnectionInfo) arg).databaseSummary());
+            }
+        }
+        sb.append("\nError Message: ");
+        sb.append(error.getMessage());
+
+        slackClient.sendMessage(sb.toString());
     }
 
 }
