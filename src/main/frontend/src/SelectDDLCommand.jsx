b4a748792a3b68c0d5a03c73e68010009915f4e5
leezzangmin <ckdals12345678@gmail.com>
2023-07-07 15:29:31 +0900

    feat: command 선택 및 add column 페이지 추가

---
+++ src/main/frontend/src/SelectDDLCommand.jsx
@@ -0,0 +1,70 @@
+import React, { useState } from 'react';
+import AddColumn from "./AddColumn";
+
+const SelectDDLCommand = () => {
+    const [selectedCommand, setSelectedCommand] = useState('');
+
+    const handleChangeCommand = (e) => {
+        setSelectedCommand(e.target.value);
+    };
+
+    return (
+        <div>
+            <label>Select Command Type:</label>
+            <select value={selectedCommand} onChange={handleChangeCommand}>
+                <option value="">Select a command type</option>
+                <option value="add_column">Add Column</option>
+                <option value="alter_column">Alter Column</option>
+                <option value="create_index">Create Index</option>
+                <option value="create_table">Create Table</option>
+                <option value="delete_column">Delete Column</option>
+                <option value="extend_varchar_column">Extend Varchar Column</option>
+                <option value="rename_column">Rename Column</option>
+            </select>
+
+            {selectedCommand === 'add_column' && (
+                <div>
+                    <AddColumn />
+                </div>
+            )}
+
+            {selectedCommand === 'alter_column' && (
+                <div>
+                    {/* Alter Column Form */}
+                </div>
+            )}
+
+            {selectedCommand === 'create_index' && (
+                <div>
+                    {/* Create Index Form */}
+                </div>
+            )}
+
+            {selectedCommand === 'create_table' && (
+                <div>
+                    {/* Create Table Form */}
+                </div>
+            )}
+
+            {selectedCommand === 'delete_column' && (
+                <div>
+                    {/* Delete Column Form */}
+                </div>
+            )}
+
+            {selectedCommand === 'extend_varchar_column' && (
+                <div>
+                    {/* Extend Varchar Column Form */}
+                </div>
+            )}
+
+            {selectedCommand === 'rename_column' && (
+                <div>
+                    {/* Rename Column Form */}
+                </div>
+            )}
+        </div>
+    );
+};
+
+export default SelectDDLCommand;
--- src/main/frontend/src/index.js
+++ src/main/frontend/src/index.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import YourComponent from "./YourComponent";
+import FetchSchemaAndTable from "./FetchSchemaAndTable";
+import DDLExecution from "./DDLExecution";
+import SelectDDLCommand from "./SelectDDLCommand";
+
+const App = () => {
+    return (
+        <div>
+            <SelectDDLCommand />
+        </div>
+    );
+};
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <YourComponent />
+      <App />
   </React.StrictMode>
 );
