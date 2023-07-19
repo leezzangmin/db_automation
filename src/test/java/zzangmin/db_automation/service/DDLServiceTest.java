7447f60194192fcb8cf267a5d050a2260a6f5954
leezzangmin <ckdals12345678@gmail.com>
2023-07-19 17:05:36 +0900

    test: extend varchar column 성공 테스트 추가

---
+++ src/test/java/zzangmin/db_automation/service/DDLServiceTest.java
@@ -0,0 +1,60 @@
+package zzangmin.db_automation.service;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import zzangmin.db_automation.client.MysqlClient;
+import zzangmin.db_automation.dto.request.ExtendVarcharColumnRequestDTO;
+import zzangmin.db_automation.entity.Column;
+import zzangmin.db_automation.entity.CommandType;
+import zzangmin.db_automation.info.DatabaseConnectionInfo;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class DDLServiceTest {
+
+    @Autowired
+    private DDLService ddlService;
+
+    @Autowired
+    private MysqlClient mysqlClient;
+
+    @Autowired
+    private AwsService awsService;
+
+    private DatabaseConnectionInfo backOfficeDatabaseConnectionInfo;
+    private String schemaName = "test_schema";
+
+    @BeforeEach
+    public void setUp() {
+        backOfficeDatabaseConnectionInfo = new DatabaseConnectionInfo("zzangmin-db", "com.mysql.cj.jdbc.Driver", "jdbc:mysql://zzangmin-db.codf49uhek24.ap-northeast-2.rds.amazonaws.com", "admin", awsService.findRdsPassword("zzangmin-db"));
+        mysqlClient.executeSQL(backOfficeDatabaseConnectionInfo, "DROP TABLE IF EXISTS test_schema.test_table");
+        mysqlClient.executeSQL(backOfficeDatabaseConnectionInfo, "CREATE TABLE test_schema.test_table (id INT NOT NULL AUTO_INCREMENT COMMENT 'asdf', name VARCHAR(45) NULL COMMENT 'name comment', PRIMARY KEY (id), KEY name(name)) COMMENT 'TABLE COMMENT'");
+    }
+
+    @AfterEach
+    public void tearDown() {
+        mysqlClient.executeSQL(backOfficeDatabaseConnectionInfo, "DROP TABLE IF EXISTS test_schema.test_table");
+    }
+
+    @DisplayName("varchar extend 성공 테스트")
+    @Test
+    void extendVarcharColumn() {
+        //given
+        ExtendVarcharColumnRequestDTO extendVarcharColumnRequestDTO = new ExtendVarcharColumnRequestDTO("test_schema", "test_table", "name", 50);
+        extendVarcharColumnRequestDTO.setCommandType(CommandType.EXTEND_VARCHAR_COLUMN);
+        System.out.println("backOfficeDatabaseConnectionInfo = " + backOfficeDatabaseConnectionInfo);
+        //when
+        ddlService.extendVarcharColumn(backOfficeDatabaseConnectionInfo, extendVarcharColumnRequestDTO);
+        //then
+        Column findColumn = mysqlClient.findColumn(backOfficeDatabaseConnectionInfo, schemaName, "test_table", "name").get();
+        int varcharLength = findColumn.injectVarcharLength();
+        Assertions.assertThat(varcharLength).isEqualTo(50);
+    }
+
+}
