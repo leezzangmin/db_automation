c50fc4a7491cbc1135dc2adc031ab52520cf90c0
leezzangmin <ckdals12345678@gmail.com>
2023-07-19 16:14:34 +0900

    feat: react describe cluster view 구현

--- src/main/frontend/src/index.js
+++ src/main/frontend/src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import SelectDDLCommand from "./ddl/SelectDDLCommand";
+import SelectDescribe from "./describe/SelectDescribe";
 
-const App = () => {
+const Index = () => {
     return (
         <div>
             <SelectDDLCommand />
+            <SelectDescribe />
         </div>
     );
 };
@@ -13,6 +15,52 @@ const App = () => {
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-      <App />
+      <Index />
   </React.StrictMode>
 );
+
+
+//
+// import React from 'react';
+// import {BrowserRouter as Router, Route, Link, Routes} from 'react-router-dom';
+// import SelectDDLCommand from "./ddl/SelectDDLCommand";
+// import SelectDescribe from "./describe/SelectDescribe";
+//
+// const Index = () => {
+//     return (
+//         <Router>
+//             <div>
+//                 <nav>
+//                     <ul>
+//                         <li>
+//                             <Link to="/">Home</Link>
+//                         </li>
+//                         <li>
+//                             <Link to="/selectDDL">Select DDL Command</Link>
+//                         </li>
+//                         <li>
+//                             <Link to="/selectDescribe">Select Describe</Link>
+//                         </li>
+//                     </ul>
+//                 </nav>
+//
+//                 <Routes>
+//                     <Route path="/selectDDL">
+//                         <SelectDDLCommand />
+//                     </Route>
+//
+//                     <Route path="/selectDescribe">
+//                         <SelectDescribe />
+//                     </Route>
+//
+//                     <Route path="/">
+//                         <h1>DB Automation Home Page</h1>
+//                     </Route>
+//
+//                 </Routes>
+//             </div>
+//         </Router>
+//     );
+// };
+//
+// export default Index;
