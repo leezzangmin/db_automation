2947f9d6fc0c8ef8ef0809e8abbbe7aec19e4018
leezzangmin <ckdals12345678@gmail.com>
2024-03-08 23:58:33 +0900

    feat: 백오피스 db 커넥션 정보 fetch 구현

--- src/main/java/zzangmin/db_automation/info/DatabaseConnectionInfo.java
+++ src/main/java/zzangmin/db_automation/info/DatabaseConnectionInfo.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.ToString;
+import org.springframework.beans.factory.annotation.Value;
 
 @ToString
 @Getter
@@ -17,7 +18,22 @@ public class DatabaseConnectionInfo {
     private String username;
     private String password;
 
+
+    public DatabaseConnectionInfo getApplicationDatabaseConnectionInfo(
+            @Value("${spring.datasource.driver-class-name}") String driverClassName,
+            @Value("${spring.datasource.url}") String url,
+            @Value("${spring.datasource.usernam}") String username,
+            @Value("${spring.datasource.password}") String password) {
+        this.databaseName = null;
+        this.driverClassName = driverClassName;
+        this.url = url;
+        this.username = username;
+        this.password = password;
+    }
+
     public String databaseSummary() {
         return this.databaseName + " (" + this.url + ")\n";
     }
+
+
 }
