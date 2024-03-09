22b4461de63de46c06283e89dc33a9b1b598abf9
leezzangmin <ckdals12345678@gmail.com>
2024-03-09 15:50:33 +0900

    test: 테스트 db커넥션정보 팩토리 생성

---
+++ src/test/java/zzangmin/db_automation/DatabaseConnectionInfoFactory.java
@@ -0,0 +1,35 @@
+package zzangmin.db_automation;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import zzangmin.db_automation.info.DatabaseConnectionInfo;
+
+
+@Component
+public class DatabaseConnectionInfoFactory {
+
+    @Value("${spring.datasource.url}")
+    private String url;
+
+    @Value("${spring.datasource.username}")
+    private String username;
+
+    @Value("${spring.datasource.password}")
+    private String password;
+
+    @Value("${spring.datasource.driver-class-name}")
+    private String driverClassName;
+
+    private String databaseName;
+
+    public DatabaseConnectionInfo createDatabaseConnectionInfo() {
+        return DatabaseConnectionInfo.builder()
+                .databaseName("inhouse")
+                .driverClassName(driverClassName)
+                .url(url)
+                .username(username)
+                .password(password)
+                .build();
+    }
+}
+
