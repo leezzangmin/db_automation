08b5a5cd94d47fedac13d826c0f06622af9029d2
leezzangmin <ckdals12345678@gmail.com>
2024-03-11 22:43:18 +0900

    feat: 데이터베이스 생성문 비교 기능 메서드 추가

---
+++ src/main/java/zzangmin/db_automation/StringMessageUtil.java
@@ -0,0 +1,15 @@
+package zzangmin.db_automation;
+
+public class StringMessageUtil {
+
+    public static String convertCreateDatabaseDifferenceMessage(String schemaName, String prodStatement, String stageStatement) {
+        StringBuilder differenceResult = new StringBuilder();
+        differenceResult.append(schemaName);
+        differenceResult.append("의 데이터베이스 생성문이 stage와 다릅니다.\nprod: ");
+        differenceResult.append(prodStatement);
+        differenceResult.append("\nstage: ");
+        differenceResult.append(stageStatement);
+        differenceResult.append("\n");
+        return differenceResult.toString();
+    }
+}
--- src/main/java/zzangmin/db_automation/schedule/tableschemacheck/DatabaseDifferenceChecker.java
+++ src/main/java/zzangmin/db_automation/schedule/tableschemacheck/DatabaseDifferenceChecker.java
@@ -3,6 +3,7 @@ package zzangmin.db_automation.schedule.tableschemacheck;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
+import zzangmin.db_automation.StringMessageUtil;
 import zzangmin.db_automation.client.MysqlClient;
 import zzangmin.db_automation.dto.DatabaseConnectionInfo;
 import zzangmin.db_automation.service.DescribeService;
@@ -19,26 +20,34 @@ public class DatabaseDifferenceChecker {
     private final MysqlClient mysqlClient;
 
 
-    public void compareDatabase(DatabaseConnectionInfo prodInfo, DatabaseConnectionInfo stageInfo) {
+    public String compareDatabase(DatabaseConnectionInfo prodInfo, DatabaseConnectionInfo stageInfo) {
+        StringBuilder differenceResult = new StringBuilder();
+
         Map<String, String> prodSchemaCreateStatements = mysqlClient.findSchemaNames(prodInfo)
                 .stream()
                 .filter(schemaName -> !DescribeService.schemaBlackList.contains(schemaName))
                 .collect(Collectors.toMap(
                         schemaName -> schemaName,
-                        schemaName -> mysqlClient.findCreateDatabaseStatement(prodInfo, schemaName)));
+                        schemaName -> mysqlClient.findCreateDatabaseStatement(prodInfo, schemaName).get()));
 
         Map<String, String> stageSchemaCreateStatements = mysqlClient.findSchemaNames(stageInfo)
                 .stream()
                 .filter(schemaName -> !DescribeService.schemaBlackList.contains(schemaName))
                 .collect(Collectors.toMap(
                         schemaName -> schemaName,
-                        schemaName -> mysqlClient.findCreateDatabaseStatement(stageInfo, schemaName)));
+                        schemaName -> mysqlClient.findCreateDatabaseStatement(stageInfo, schemaName).get()));
 
         for (String prodSchemaName : prodSchemaCreateStatements.keySet()) {
-            String prodStatement = mysqlClient.findCreateDatabaseStatement(prodInfo, prodSchemaName);
+            String prodStatement = prodSchemaCreateStatements.get(prodSchemaName);
+            String stageStatement = stageSchemaCreateStatements.get(prodSchemaName);
 
+            if (!prodStatement.equals(stageStatement)) {
+                differenceResult.append(StringMessageUtil.convertCreateDatabaseDifferenceMessage(prodSchemaName, prodStatement, stageStatement));
+            }
         }
 
+        return differenceResult.toString();
     }
+
 
 }
