6dd66cbe652b693a0d98efebd989be60d7129dab
leezzangmin <ckdals12345678@gmail.com>
2024-03-12 22:40:09 +0900

    test: TableDifferenceChecker 관련 테스트 4개 추가

--- src/test/java/zzangmin/db_automation/schedule/tableschemacheck/TableDifferenceCheckerTest.java
+++ src/test/java/zzangmin/db_automation/schedule/tableschemacheck/TableDifferenceCheckerTest.java
@@ -1,6 +1,8 @@
 package zzangmin.db_automation.schedule.tableschemacheck;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
@@ -10,6 +12,7 @@ import zzangmin.db_automation.client.MysqlClient;
 import zzangmin.db_automation.dto.DatabaseConnectionInfo;
 import zzangmin.db_automation.entity.Column;
 import zzangmin.db_automation.entity.Table;
+import zzangmin.db_automation.testfactory.EntityFactory;
 
 import java.util.List;
 
@@ -32,6 +35,7 @@ class TableDifferenceCheckerTest {
         tableDifferenceChecker = new TableDifferenceChecker(mysqlClient);
     }
 
+    @DisplayName("테이블 조회 결과가 같으면 문자열을 반환하지 않아야 한다.")
     @Test
     void compareTableSchema() {
         // given
@@ -40,22 +44,81 @@ class TableDifferenceCheckerTest {
 
         when(mysqlClient.findSchemaNames(eq(prod))).thenReturn(List.of("test"));
         when(mysqlClient.findTableNames(any(), eq("test"))).thenReturn(List.of("test_table"));
-        when(mysqlClient.findTables(eq(prod), eq("test"), any())).thenReturn(List.of(
-                Table.builder()
-                        .tableName("test_table")
-                        .tableEngine("innoDB")
-                        .columns(List.of(new Column()))
-                        .build()));
-        when(mysqlClient.findTables(eq(stage), eq("test"), any())).thenReturn(List.of(
-                Table.builder()
-                        .tableName("test_table")
-                        .tableEngine("sdf")
-                        .build()));
+        when(mysqlClient.findTables(eq(prod), eq("test"), any())).thenReturn(List.of(EntityFactory.generateBasicTable("test_table")));
+        when(mysqlClient.findTables(eq(stage), eq("test"), any())).thenReturn(List.of(EntityFactory.generateBasicTable("test_table")));
         // when
         String s = tableDifferenceChecker.compareTableSchema(prod, stage);
 
         // then
-        System.out.println("s = " + s);
+        Assertions.assertThat(s).isBlank();
+
+    }
+
+    @DisplayName("테이블 조회 결과가 없으면 '찾을 수 없습니다.'를 포함한 문자열이 반환되어야 한다.")
+    @Test
+    void compareTableSchema_notable() {
+        // given
+        DatabaseConnectionInfo prod = new DatabaseConnectionInfo("prod", "com.mysql.cj.jdbc.Driver", "testendpoint1", "admin", "123*");
+        DatabaseConnectionInfo stage = new DatabaseConnectionInfo("inhouse", "com.mysql.cj.jdbc.Driver", "testendpoint1", "admin", "123*");
+
+        when(mysqlClient.findSchemaNames(eq(prod))).thenReturn(List.of("test"));
+        when(mysqlClient.findTableNames(any(), eq("test"))).thenReturn(List.of("test_table"));
+        when(mysqlClient.findTables(eq(prod), eq("test"), any())).thenReturn(List.of(EntityFactory.generateBasicTable("test_table")));
+        when(mysqlClient.findTables(eq(stage), eq("test"), any())).thenReturn(List.of(EntityFactory.generateBasicTable("test_tablee")));
+        // when
+        String s = tableDifferenceChecker.compareTableSchema(prod, stage);
+
+        // then
+        Assertions.assertThat(s).contains("찾을 수 없습니다.");
+
+    }
+
+    @DisplayName("컬럼 개수가 다르면 '컬럼 개수가 다릅니다'를 포함한 문자열이 반환되어야 한다.")
+    @Test
+    void compareTableSchema_different_differentcolumnsize() {
+        // given
+        DatabaseConnectionInfo prod = new DatabaseConnectionInfo("prod", "com.mysql.cj.jdbc.Driver", "testendpoint1", "admin", "123*");
+        DatabaseConnectionInfo stage = new DatabaseConnectionInfo("inhouse", "com.mysql.cj.jdbc.Driver", "testendpoint1", "admin", "123*");
+
+        Table prodTable = EntityFactory.generateBasicTable("basic_table");
+        Table stageTable = EntityFactory.generateBasicTable("basic_table");
+        stageTable.addColumns(List.of(EntityFactory.generateBasicColumn("extra_column")));
+
+        when(mysqlClient.findSchemaNames(eq(prod))).thenReturn(List.of("test"));
+        when(mysqlClient.findTableNames(any(), eq("test"))).thenReturn(List.of("test_table"));
+        when(mysqlClient.findTables(eq(prod), eq("test"), any())).thenReturn(List.of(prodTable));
+        when(mysqlClient.findTables(eq(stage), eq("test"), any())).thenReturn(List.of(stageTable));
+        // when
+        String s = tableDifferenceChecker.compareTableSchema(prod, stage);
+
+        // then
+        Assertions.assertThat(s).contains("컬럼 개수가 다릅니다");
+
+    }
+
+    @DisplayName("컬럼 개수가 다르면 '컬럼 개수가 다릅니다'를 포함한 문자열이 반환되어야 한다.")
+    @Test
+    void compareTableSchema_different_differentcolumn() {
+        // given
+        DatabaseConnectionInfo prod = new DatabaseConnectionInfo("prod", "com.mysql.cj.jdbc.Driver", "testendpoint1", "admin", "123*");
+        DatabaseConnectionInfo stage = new DatabaseConnectionInfo("inhouse", "com.mysql.cj.jdbc.Driver", "testendpoint1", "admin", "123*");
+
+        Table prodTable = EntityFactory.generateBasicTable("basic_table");
+        Table stageTable = EntityFactory.generateBasicTable("basic_table");
+        prodTable.addColumns(List.of(new Column("column_name", "varchar(123)", true, null, false, false, "comment", "utf8mb4", "utf8mb4_0900_ai_ci")));
+        stageTable.addColumns(List.of(new Column("column_name", "varchar(124)", true, null, false, false, "asdfcomment", "utf8mb4", "utf8mb4_0900_ai_ci")));
+
+
+        when(mysqlClient.findSchemaNames(eq(prod))).thenReturn(List.of("test"));
+        when(mysqlClient.findTableNames(any(), eq("test"))).thenReturn(List.of("test_table"));
+        when(mysqlClient.findTables(eq(prod), eq("test"), any())).thenReturn(List.of(prodTable));
+        when(mysqlClient.findTables(eq(stage), eq("test"), any())).thenReturn(List.of(stageTable));
+        // when
+        String s = tableDifferenceChecker.compareTableSchema(prod, stage);
+
+        // then
+        Assertions.assertThat(s).contains("타입이 다릅니다");
+        Assertions.assertThat(s).contains("설명이 다릅니다");
 
     }
 }
