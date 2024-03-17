7b5a8d0698e5cc8f03ab990bec2fb9c173f788cb
leezzangmin <ckdals12345678@gmail.com>
2024-03-17 19:15:49 +0900

    feat: slack 메세지 전송 빈 문자열 검사 추가

--- src/main/java/zzangmin/db_automation/client/SlackClient.java
+++ src/main/java/zzangmin/db_automation/client/SlackClient.java
@@ -1,6 +1,7 @@
 package zzangmin.db_automation.client;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
@@ -9,6 +10,7 @@ import zzangmin.db_automation.dto.response.SlackMessageDTO;
 
 import org.springframework.http.HttpHeaders;
 
+@Slf4j
 @RequiredArgsConstructor
 @Component
 public class SlackClient {
@@ -17,10 +19,14 @@ public class SlackClient {
     private final RestTemplate restTemplate;
 
     public void sendMessage(String message) {
+        if (message.isBlank()) {
+            return;
+        }
+
         SlackMessageDTO slackMessage = new SlackMessageDTO(message);
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
-
+        log.info("message: {}", message);
         HttpEntity<SlackMessageDTO> entity = new HttpEntity<>(slackMessage, headers);
         restTemplate.postForEntity(slackWebhookUrl, entity, String.class);
     }
