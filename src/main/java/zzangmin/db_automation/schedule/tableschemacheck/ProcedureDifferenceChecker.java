2f215e6bcd757e825dc29d3e23f62145375432db
leezzangmin <ckdals12345678@gmail.com>
2024-03-17 19:12:47 +0900

    feat: 로그 추가

--- src/main/java/zzangmin/db_automation/schedule/tableschemacheck/ProcedureDifferenceChecker.java
+++ src/main/java/zzangmin/db_automation/schedule/tableschemacheck/ProcedureDifferenceChecker.java
@@ -43,7 +43,7 @@ public class ProcedureDifferenceChecker {
                 differenceResult.append(sourceProcedure.reportDifference(replicaProcedure));
             }
         }
-
+        log.info("Procedure Result: {}", differenceResult.toString());
         return differenceResult.toString();
     }
 }
