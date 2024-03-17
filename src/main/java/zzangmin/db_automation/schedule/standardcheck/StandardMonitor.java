b91a300144b8eb7630def3a806665da16820ae49
leezzangmin <ckdals12345678@gmail.com>
2024-03-17 18:15:40 +0900

    feat: 표준 모니터에 태그검사 추가

--- src/main/java/zzangmin/db_automation/schedule/standardcheck/StandardMonitor.java
+++ src/main/java/zzangmin/db_automation/schedule/standardcheck/StandardMonitor.java
@@ -1,6 +1,7 @@
 package zzangmin.db_automation.schedule.standardcheck;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import zzangmin.db_automation.client.SlackClient;
 
@@ -12,21 +13,25 @@ public class StandardMonitor {
     private final InstanceCreationStandardChecker instanceCreationStandardChecker;
     private final ParameterStandardChecker parameterStandardChecker;
     private final SchemaStandardChecker schemaStandardChecker;
+    private final TagStandardChecker tagStandardChecker;
     private final SlackClient slackClient;
 
-    //@Scheduled(fixedDelay = 1000000)
+    @Scheduled(fixedDelay = 1000000)
     public void checkStandard() {
+        StringBuilder standardCheckResult = new StringBuilder();
+
         String parameterCheckResult = parameterStandardChecker.checkParameterStandard();
         String clusterCreationStandardResult = clusterCreationStandardChecker.checkClusterCreationStandard();
         String instanceCreationStandardResult = instanceCreationStandardChecker.checkInstanceCreationStandard();
         String schemaStandardResult = schemaStandardChecker.checkSchemaStandard();
+        String tagStandardResult = tagStandardChecker.checkTagStandard();
 
-        StringBuilder sb = new StringBuilder();
-        sb.append(parameterCheckResult);
-        sb.append(clusterCreationStandardResult);
-        sb.append(instanceCreationStandardResult);
-        sb.append(schemaStandardResult);
+        standardCheckResult.append(parameterCheckResult);
+        standardCheckResult.append(clusterCreationStandardResult);
+        standardCheckResult.append(instanceCreationStandardResult);
+        standardCheckResult.append(schemaStandardResult);
+        standardCheckResult.append(tagStandardResult);
 
-        slackClient.sendMessage(sb.toString());
+        slackClient.sendMessage(standardCheckResult.toString());
     }
 }
