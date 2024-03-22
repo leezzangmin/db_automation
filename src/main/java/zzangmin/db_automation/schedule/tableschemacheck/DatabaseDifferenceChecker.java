5d099b7c33a0bb0fbb0146e026cfb23aa71bb0a2
leezzangmin <ckdals12345678@gmail.com>
2024-03-22 20:47:32 +0900

    feat: create database 문 저장 기능 추가
    - 환경이 prod 일 때만 호출되는 메서드
    - 환경이 stage 일 때는 저장된 데이터를 가져와서 비교

--- src/main/java/zzangmin/db_automation/schedule/tableschemacheck/DatabaseDifferenceChecker.java
+++ src/main/java/zzangmin/db_automation/schedule/tableschemacheck/DatabaseDifferenceChecker.java
@@ -3,7 +3,8 @@ package zzangmin.db_automation.schedule.tableschemacheck;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
-import zzangmin.db_automation.StringMessageUtil;
+import zzangmin.db_automation.service.SchemaService;
+import zzangmin.db_automation.util.StringMessageUtil;
 import zzangmin.db_automation.client.MysqlClient;
 import zzangmin.db_automation.dto.DatabaseConnectionInfo;
 import zzangmin.db_automation.service.DescribeService;
@@ -18,7 +19,7 @@ import java.util.stream.Collectors;
 public class DatabaseDifferenceChecker {
 
     private final MysqlClient mysqlClient;
-
+    private final SchemaService schemaService;
 
     public String compareDatabase(DatabaseConnectionInfo sourceInfo, DatabaseConnectionInfo replicaInfo) {
         StringBuilder differenceResult = new StringBuilder();
@@ -48,6 +49,20 @@ public class DatabaseDifferenceChecker {
 
         log.info("DatabaseDifferenceChecker Result: {}", differenceResult.toString());
         return differenceResult.toString();
+    }
+
+    public void saveDatabase(DatabaseConnectionInfo databaseConnectionInfo) throws Exception {
+        log.info("database: {}", databaseConnectionInfo);
+        String serviceName = databaseConnectionInfo.findServiceName();
+        log.info("serviceName: {}", serviceName);
+        Map<String, String> schemaCreateStatements = mysqlClient.findSchemaNames(databaseConnectionInfo)
+                .stream()
+                .filter(schemaName -> !DescribeService.schemaBlackList.contains(schemaName))
+                .collect(Collectors.toMap(
+                        schemaName -> schemaName,
+                        schemaName -> mysqlClient.findCreateDatabaseStatement(databaseConnectionInfo, schemaName).get()));
+        log.info("schemaCreateStatements: {}", schemaCreateStatements);
+        schemaService.saveDatabases(serviceName, schemaCreateStatements);
     }
 
 
