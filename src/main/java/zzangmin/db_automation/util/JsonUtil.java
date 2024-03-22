669a920534aa47c9b37cb4e6e6f5edcfc0249d8c
leezzangmin <ckdals12345678@gmail.com>
2024-03-22 17:09:27 +0900

    feat: json uitl 클래스 및 직렬화, 역직렬화 메서드 추가

---
+++ src/main/java/zzangmin/db_automation/util/JsonUtil.java
@@ -0,0 +1,18 @@
+package zzangmin.db_automation.util;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class JsonUtil {
+    private static ObjectMapper objectMapper = new ObjectMapper();
+
+    public static String toJson(Object object) throws JsonProcessingException {
+        return objectMapper.writeValueAsString(object);
+    }
+
+    public static Object toObject(String jsonString) throws JsonProcessingException {
+        Object o = objectMapper.readValue(jsonString, Object.class);
+        return o;
+    }
+
+}
