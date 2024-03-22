5595dd457e5363c33920ebe12b0502c317cf79d1
leezzangmin <ckdals12345678@gmail.com>
2024-03-22 18:26:47 +0900

    docs: schema 테이블 추가 문서화

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,8 +1,8 @@
 
-CREATE DATABASE IF NOT EXISTS automation_change_history;
+CREATE DATABASE IF NOT EXISTS back_office;
 CREATE DATABASE IF NOT EXISTS test_schema;
 
-CREATE TABLE IF NOT EXISTS automation_change_history.change_history (
+CREATE TABLE IF NOT EXISTS back_office.change_history (
     id bigint primary key auto_increment,
     command_type varchar(64) not null,
     database_identifier varchar(255) not null,
@@ -11,4 +11,12 @@ CREATE TABLE IF NOT EXISTS automation_change_history.change_history (
     doer varchar(255) not null,
     do_date_time datetime not null,
     change_content text not null
+);
+
+CREATE TABLE IF NOT EXISTS back_office.schema (
+    id bigint primary key auto_increment,
+    schema_type varchar(64) not null,
+    schema_name varchar(64) not null,
+    service_name varchar(64) not null,
+    encrypted_json_string mediumtext not null
 );
