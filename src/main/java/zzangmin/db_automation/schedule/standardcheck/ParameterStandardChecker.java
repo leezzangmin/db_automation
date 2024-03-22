43508ce0836427ace9349663120f3745935d4ba0
leezzangmin <ckdals12345678@gmail.com>
2024-03-22 13:45:50 +0900

    feat: parameter 표준 체크 시 db parameter 그룹 값도 검사하도록 추가

--- src/main/java/zzangmin/db_automation/schedule/standardcheck/ParameterStandardChecker.java
+++ src/main/java/zzangmin/db_automation/schedule/standardcheck/ParameterStandardChecker.java
@@ -3,7 +3,6 @@ package zzangmin.db_automation.schedule.standardcheck;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
-import software.amazon.awssdk.services.rds.model.DescribeDbClusterParametersResponse;
 import software.amazon.awssdk.services.rds.model.Parameter;
 import zzangmin.db_automation.schedule.standardcheck.standardvalue.ParameterGroupStandard;
 import zzangmin.db_automation.service.AwsService;
@@ -19,18 +18,27 @@ public class ParameterStandardChecker {
 
     public String checkParameterStandard() {
         StringBuilder sb = new StringBuilder();
-        List<String> dbParameterGroupNames = awsService.findDbParameterGroupNames();
         List<String> clusterParameterGroupNames = awsService.findClusterParameterGroupNames();
+        List<String> dbParameterGroupNames = awsService.findDbParameterGroupNames();
         log.info("dbParameterGroupNames: {}", dbParameterGroupNames);
         log.info("clusterParameterGroupNames: {}", clusterParameterGroupNames);
 
-        for (String parameterGroupName : parameterGroupNames) {
-            DescribeDbClusterParametersResponse parameterGroup = awsService.findParameterGroup(parameterGroupName);
+        for (String parameterGroupName : clusterParameterGroupNames) {
+            List<Parameter> clusterParameters = awsService.findClusterParameterGroupParameters(parameterGroupName);
 
-            List<Parameter> parameters = parameterGroup.parameters();
-            for (Parameter parameter : parameters) {
+            for (Parameter parameter : clusterParameters) {
                 if (!ParameterGroupStandard.findStandardValue(parameter.parameterName()).equals(parameter.parameterValue())) {
-                    sb.append(String.format("\nParameter Group Name: %s, 비표준 파라미터명: %s, 표준값: %s, 현재값: %s", parameterGroupName, parameter.parameterName(), ParameterGroupStandard.findStandardValue(parameter.parameterName()), parameter.parameterValue()));
+                    sb.append(String.format("\nCluster Parameter Group Name: %s, 비표준 파라미터명: %s, 표준값: %s, 현재값: %s", parameterGroupName, parameter.parameterName(), ParameterGroupStandard.findStandardValue(parameter.parameterName()), parameter.parameterValue()));
+                }
+            }
+        }
+
+        for (String parameterGroupName : dbParameterGroupNames) {
+            List<Parameter> dbParameters = awsService.findDbParameterGroupParameters(parameterGroupName);
+
+            for (Parameter parameter : dbParameters) {
+                if (!ParameterGroupStandard.findStandardValue(parameter.parameterName()).equals(parameter.parameterValue())) {
+                    sb.append(String.format("\nDB Parameter Group Name: %s, 비표준 파라미터명: %s, 표준값: %s, 현재값: %s", parameterGroupName, parameter.parameterName(), ParameterGroupStandard.findStandardValue(parameter.parameterName()), parameter.parameterValue()));
                 }
             }
         }
