962eb79b2348076cf4330c37ee957a9bbc653ea9
leezzangmin <ckdals12345678@gmail.com>
2024-03-22 18:29:44 +0900

    feat: Table, CreateDatabase 저장 서비스 메서드 추가

---
+++ src/main/java/zzangmin/db_automation/service/SchemaService.java
@@ -0,0 +1,69 @@
+package zzangmin.db_automation.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import zzangmin.db_automation.entity.Schema;
+import zzangmin.db_automation.entity.SchemaType;
+import zzangmin.db_automation.entity.Table;
+import zzangmin.db_automation.repository.SchemaRepository;
+import zzangmin.db_automation.util.EncryptionUtil;
+import zzangmin.db_automation.util.JsonUtil;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@Slf4j
+@RequiredArgsConstructor
+@Service
+public class SchemaService {
+
+    private final SchemaRepository schemaRepository;
+
+    @Transactional
+    public void saveTable(String serviceName, Table table) throws Exception {
+        String encryptedJsonTable = makeEncryptedJsonString(table);
+
+        Schema schema = Schema.builder()
+                .schemaType(SchemaType.TABLE)
+                .schemaName(table.getTableName())
+                .serviceName(serviceName)
+                .encryptedJsonString(encryptedJsonTable)
+                .build();
+
+        schemaRepository.save(schema);
+    }
+
+    @Transactional
+    public void saveDatabases(String serviceName, Map<String, String> schemaCreateStatements) throws Exception {
+        List<Schema> schemas = new ArrayList<>();
+        for (String schemaName : schemaCreateStatements.keySet()) {
+            String encryptedJsonCreateDatabase = makeEncryptedJsonString(schemaCreateStatements.get(schemaName));
+            Schema schema = Schema.builder()
+                    .schemaType(SchemaType.DATABASE)
+                    .schemaName(schemaName)
+                    .serviceName(serviceName)
+                    .encryptedJsonString(encryptedJsonCreateDatabase)
+                    .build();
+            schemas.add(schema);
+        }
+
+        schemaRepository.saveAll(schemas);
+    }
+
+    private String makeEncryptedJsonString(Object object) throws Exception {
+        String jsonString = JsonUtil.toJson(object);
+        String encryptedJsonString = EncryptionUtil.encrypt(jsonString);
+        return encryptedJsonString;
+    }
+
+    private Object encryptedJsonStringToObject(String encryptedJsonString) throws Exception {
+        String jsonString = EncryptionUtil.decrypt(encryptedJsonString);
+        Object object = JsonUtil.toObject(jsonString);
+
+        return object;
+    }
+}
