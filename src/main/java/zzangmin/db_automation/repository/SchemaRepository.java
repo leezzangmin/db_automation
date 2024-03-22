fd15f5bfd16f8c8ca60c5619d1027e32cf909a2c
leezzangmin <ckdals12345678@gmail.com>
2024-03-22 18:29:15 +0900

    feat: SchemaRepository 조회 쿼리 메서드 추가

--- src/main/java/zzangmin/db_automation/repository/SchemaRepository.java
+++ src/main/java/zzangmin/db_automation/repository/SchemaRepository.java
@@ -1,12 +1,16 @@
 package zzangmin.db_automation.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import zzangmin.db_automation.entity.Schema;
+import zzangmin.db_automation.entity.SchemaType;
 
+import java.util.List;
 
 @Repository
 public interface SchemaRepository extends JpaRepository<Schema, Long> {
 
-
+    @Query
+    List<Schema> findByServiceNameAndSchemaType(String serviceName, SchemaType schemaType);
 }
