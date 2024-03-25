2c9d758c5beb7071dc7054f89a9343e466b4cfce
leezzangmin <ckdals12345678@gmail.com>
2024-03-25 14:48:26 +0900

    test: 깨지는 테스트 수정(의존성 추가)

--- src/test/java/zzangmin/db_automation/schedule/tableschemacheck/DatabaseDifferenceCheckerTest.java
+++ src/test/java/zzangmin/db_automation/schedule/tableschemacheck/DatabaseDifferenceCheckerTest.java
@@ -7,6 +7,7 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import zzangmin.db_automation.client.MysqlClient;
 import zzangmin.db_automation.dto.DatabaseConnectionInfo;
+import zzangmin.db_automation.service.SchemaObjectService;
 
 import java.util.List;
 import java.util.Optional;
@@ -20,13 +21,14 @@ class DatabaseDifferenceCheckerTest {
 
     @Mock
     private MysqlClient mysqlClient;
-
+    @Mock
+    private SchemaObjectService schemaObjectService;
     private DatabaseDifferenceChecker databaseDifferenceChecker;
 
     @BeforeEach
     void setUp() {
         MockitoAnnotations.openMocks(this);
-        databaseDifferenceChecker = new DatabaseDifferenceChecker(mysqlClient);
+        databaseDifferenceChecker = new DatabaseDifferenceChecker(mysqlClient, schemaObjectService);
     }
 
     @DisplayName("compareDatabase로 database 생성 구문이 같으면 빈 String이 반환된다")
