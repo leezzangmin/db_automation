44a6b298234629ae79aab215f74b81ee5462def5
leezzangmin <ckdals12345678@gmail.com>
2024-03-26 14:05:27 +0900

    feat: slack message 10000자 단위로 잘라서 보내도록 변경

--- src/main/java/zzangmin/db_automation/client/SlackClient.java
+++ src/main/java/zzangmin/db_automation/client/SlackClient.java
@@ -17,17 +17,27 @@ public class SlackClient {
 
     private final static String slackWebhookUrl = System.getenv("SLACK_WEBHOOK_URL");
     private final RestTemplate restTemplate;
+    private final int MAX_MESSAGE_SIZE = 10000;
 
     public void sendMessage(String message) {
         if (message.isBlank()) {
             return;
         }
 
-        SlackMessageDTO slackMessage = new SlackMessageDTO(message);
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_JSON);
-        log.info("message: {}", message);
-        HttpEntity<SlackMessageDTO> entity = new HttpEntity<>(slackMessage, headers);
-        restTemplate.postForEntity(slackWebhookUrl, entity, String.class);
+        for (int start = 0; start < message.length(); start += MAX_MESSAGE_SIZE) {
+            int end = Math.min(message.length(), start + MAX_MESSAGE_SIZE);
+            String messageChunk = getChunk(message, start, end);
+            log.info("Sending message chunk: {}", messageChunk);
+
+            SlackMessageDTO slackMessage = new SlackMessageDTO(messageChunk);
+            HttpHeaders headers = new HttpHeaders();
+            headers.setContentType(MediaType.APPLICATION_JSON);
+            HttpEntity<SlackMessageDTO> entity = new HttpEntity<>(slackMessage, headers);
+            restTemplate.postForEntity(slackWebhookUrl, entity, String.class);
+        }
+    }
+
+    private String getChunk(String message, int start, int end) {
+        return message.substring(start, end);
     }
 }
