f9b314ae85a00d8bc30ff8a82ab4969d6b5a41f5
leezzangmin <ckdals12345678@gmail.com>
2024-03-29 15:16:18 +0900

    feat: profile 변수 static으로 변경

--- src/main/java/zzangmin/db_automation/util/ProfileUtil.java
+++ src/main/java/zzangmin/db_automation/util/ProfileUtil.java
@@ -1,12 +1,16 @@
 package zzangmin.db_automation.util;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ProfileUtil {
 
-    @Value("${spring.profiles.active}")
-    public String CURRENT_ENVIRONMENT_PROFILE;
+    public static String CURRENT_ENVIRONMENT_PROFILE;
 
+    @Autowired
+    public void setCURRENT_ENVIRONMENT_PROFILE(@Value("${spring.profiles.active}")String profile) {
+        this.CURRENT_ENVIRONMENT_PROFILE = profile;
+    }
 }
