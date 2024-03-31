17217155462558288ef4ed4faaaef5c31250685f
leezzangmin <ckdals12345678@gmail.com>
2024-03-31 14:29:55 +0900

    feat: 개발팀 쿼리 요청 컨트롤러 추가

---
+++ src/main/java/zzangmin/db_automation/controller/QueryRequestController.java
@@ -0,0 +1,12 @@
+package zzangmin.db_automation.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RequiredArgsConstructor
+@RestController
+public class QueryRequestController {
+
+}
