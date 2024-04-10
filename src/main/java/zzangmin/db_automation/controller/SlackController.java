4e69d57d201358bf0a27ba0bd28d1a76be2c4c12
leezzangmin <ckdals12345678@gmail.com>
2024-04-10 19:02:10 +0900

    feat: slacktest 메세지전송 controller 메서드 작성
    - slackclient 레거시 제거 필요해보임

--- src/main/java/zzangmin/db_automation/controller/SlackController.java
+++ src/main/java/zzangmin/db_automation/controller/SlackController.java
@@ -1,12 +1,19 @@
 package zzangmin.db_automation.controller;
 
+import com.slack.api.Slack;
+import com.slack.api.methods.MethodsClient;
+import com.slack.api.methods.SlackApiException;
+import com.slack.api.methods.request.chat.ChatPostMessageRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -21,5 +28,25 @@ public class SlackController {
 
 
         return ResponseEntity.ok(true);
+    }
+
+    @GetMapping("/slacktest")
+    public void sendSlackMessage(String message, String channel) {
+
+        String channelAddress = channel;
+
+        try{
+            MethodsClient methods = Slack.getInstance().methods(slackToken);
+
+            ChatPostMessageRequest request = ChatPostMessageRequest.builder()
+                    .channel(channelAddress)
+                    .text(message)
+                    .build();
+
+            methods.chatPostMessage(request);
+
+        } catch (SlackApiException | IOException e) {
+            log.error(e.getMessage());
+        }
     }
 }
