04ae7e4887a1234e2b143b5b50a4dafb80587eeb
leezzangmin <ckdals12345678@gmail.com>
2024-04-10 20:37:16 +0900

    feat: layoutblock 슬랙 전송 연습코드 작성

--- src/main/java/zzangmin/db_automation/controller/SlackController.java
+++ src/main/java/zzangmin/db_automation/controller/SlackController.java
@@ -4,6 +4,8 @@ import com.slack.api.Slack;
 import com.slack.api.methods.MethodsClient;
 import com.slack.api.methods.SlackApiException;
 import com.slack.api.methods.request.chat.ChatPostMessageRequest;
+import com.slack.api.methods.response.chat.ChatPostMessageResponse;
+import com.slack.api.model.block.LayoutBlock;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
@@ -14,6 +16,14 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.slack.api.model.block.Blocks.*;
+import static com.slack.api.model.block.composition.BlockCompositions.markdownText;
+import static com.slack.api.model.block.composition.BlockCompositions.plainText;
+import static com.slack.api.model.block.element.BlockElements.asElements;
+import static com.slack.api.model.block.element.BlockElements.button;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -34,6 +44,24 @@ public class SlackController {
     public void sendSlackMessage(String message, String channel) {
 
         String channelAddress = channel;
+        List<LayoutBlock> layoutBlocks = new ArrayList<>();
+        // 텍스트를 남길 SectionBlock 입니다.
+        layoutBlocks.add(section(section -> section.text(markdownText("새로운 배송팁이 등록되었습니다."))));
+        // Action과 텍스트를 구분하기 위한 Divider 입니다.
+        layoutBlocks.add(divider());
+        // ActionBlock에 승인 버튼과 거부 버튼을 추가 하였습니다.
+        layoutBlocks.add(
+                actions(actions -> actions
+                        .elements(asElements(
+                                button(b -> b.text(plainText(pt -> pt.emoji(true).text("승인")))
+                                        .value("deliveryTip.getSeq().toString()")
+                                        .style("primary")
+                                        .text(plainText("ddd"))
+                                        .actionId("shortcut-test1")
+                                )
+                        ))
+                )
+        );
 
         try{
             MethodsClient methods = Slack.getInstance().methods(slackToken);
@@ -41,12 +69,16 @@ public class SlackController {
             ChatPostMessageRequest request = ChatPostMessageRequest.builder()
                     .channel(channelAddress)
                     .text(message)
+                    .blocks(layoutBlocks)
                     .build();
 
-            methods.chatPostMessage(request);
+            ChatPostMessageResponse chatPostMessageResponse = methods.chatPostMessage(request);
+            log.info("ChatPostMessageResponse: {}", chatPostMessageResponse);
 
         } catch (SlackApiException | IOException e) {
             log.error(e.getMessage());
         }
     }
+
+
 }
