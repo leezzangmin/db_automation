78dab25316e097cb6037c29c5d2e1a07b2d55c75
leezzangmin <ckdals12345678@gmail.com>
2024-04-13 00:59:13 +0900

    feat: slack config 생성 및 관련 변수 이동

---
+++ src/main/java/zzangmin/db_automation/config/SlackConfig.java
@@ -0,0 +1,28 @@
+package zzangmin.db_automation.config;
+
+import com.slack.api.Slack;
+import com.slack.api.methods.MethodsClient;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@RequiredArgsConstructor
+@Configuration
+public class SlackConfig {
+
+    @Value("${slack.token}")
+    public String slackToken;
+
+    @Value("${slack.webbhook-url}")
+    private String SLACK_WEBHOOK_URL;
+
+    public static String DEFAULT_CHANNEL_ID = "C04282D342D";
+
+    public static final int MAX_MESSAGE_SIZE = 10000;
+
+    @Bean
+    public MethodsClient methodsClient() {
+        return Slack.getInstance().methods(slackToken);
+    }
+}
