cae5bc00ca1adb19c7022b5eb644c3d7cf8b86a3
leezzangmin <ckdals12345678@gmail.com>
2024-04-13 00:57:47 +0900

    feat: slack 메세지 전송 메서드 이동

--- src/main/java/zzangmin/db_automation/service/SlackService.java
+++ src/main/java/zzangmin/db_automation/service/SlackService.java
@@ -1,15 +1,22 @@
 package zzangmin.db_automation.service;
 
+import com.slack.api.Slack;
+import com.slack.api.methods.MethodsClient;
+import com.slack.api.methods.request.chat.ChatPostMessageRequest;
+import com.slack.api.methods.response.chat.ChatPostMessageResponse;
 import com.slack.api.model.block.composition.OptionObject;
 import com.slack.api.model.block.element.StaticSelectElement;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import static com.slack.api.model.block.composition.BlockCompositions.plainText;
+import static zzangmin.db_automation.config.SlackConfig.DEFAULT_CHANNEL_ID;
+import static zzangmin.db_automation.config.SlackConfig.MAX_MESSAGE_SIZE;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -17,8 +24,11 @@ import static com.slack.api.model.block.composition.BlockCompositions.plainText;
 public class SlackService {
 
     private final DescribeService describeService;
+    private final MethodsClient slackClient;
 
     public StaticSelectElement findClusterSelects() {
+
+
         List<OptionObject> selectOptions = describeService.findDBMSNames()
                 .getDbmsNames()
                 .stream()
@@ -31,11 +41,39 @@ public class SlackService {
         StaticSelectElement clusterSelects = StaticSelectElement.builder()
                 .options(selectOptions)
                 .placeholder(plainText("select cluster"))
-                .actionId("cluster")
+                .actionId("selectedClusterName")
                 .build();
 
         return clusterSelects;
     }
 
+    public StaticSelectElement findSchemaSelects() {
+        List<StaticSelectElement> schemaSelects = new ArrayList<>();
+
+        return null;
+    }
+
+    public void sendMessage(String message) {
+        if (message.isBlank()) {
+            return;
+        }
+        log.info("slack message: {}", message);
+        for (int start = 0; start < message.length(); start += MAX_MESSAGE_SIZE) {
+            int end = Math.min(message.length(), start + MAX_MESSAGE_SIZE);
+            String messageChunk = message.substring(start, end);
+
+            ChatPostMessageRequest request = ChatPostMessageRequest.builder()
+                    .channel(DEFAULT_CHANNEL_ID)
+                    .text(messageChunk)
+                    .build();
+            ChatPostMessageResponse chatPostMessageResponse = null;
+            try {
+                chatPostMessageResponse = slackClient.chatPostMessage(request);
+            } catch (Exception e) {
+                log.info(e.getMessage());
+            }
+            log.info("chatPostMessageResponse: {]", chatPostMessageResponse);
+        }
+    }
 
 }
