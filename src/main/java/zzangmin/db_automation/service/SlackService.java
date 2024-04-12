ad29be450a25908325aac18f89ff80c6d36e6242
leezzangmin <ckdals12345678@gmail.com>
2024-04-12 22:50:25 +0900

    feat: dbms name 드랍다운 셀렉트 버튼 생성 서비스메서드 작성

--- src/main/java/zzangmin/db_automation/service/SlackService.java
+++ src/main/java/zzangmin/db_automation/service/SlackService.java
@@ -1,9 +1,41 @@
 package zzangmin.db_automation.service;
 
+import com.slack.api.model.block.composition.OptionObject;
+import com.slack.api.model.block.element.StaticSelectElement;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static com.slack.api.model.block.composition.BlockCompositions.plainText;
+
 @Slf4j
+@RequiredArgsConstructor
 @Service
 public class SlackService {
+
+    private final DescribeService describeService;
+
+    public StaticSelectElement findClusterSelects() {
+        List<OptionObject> selectOptions = describeService.findDBMSNames()
+                .getDbmsNames()
+                .stream()
+                .map(dbmsName -> OptionObject.builder()
+                        .text(plainText(dbmsName))
+                        .value(dbmsName)
+                        .build()
+                )
+                .collect(Collectors.toList());
+        StaticSelectElement clusterSelects = StaticSelectElement.builder()
+                .options(selectOptions)
+                .placeholder(plainText("select cluster"))
+                .actionId("cluster")
+                .build();
+
+        return clusterSelects;
+    }
+
+
 }
