87e91c987217a6b9661354fc3b68cfd0c45c3eb1
leezzangmin <ckdals12345678@gmail.com>
2024-04-14 16:47:49 +0900

    docs: TODO 추가 - 변수 환경변수화

--- src/main/java/zzangmin/db_automation/config/SlackConfig.java
+++ src/main/java/zzangmin/db_automation/config/SlackConfig.java
@@ -17,7 +17,7 @@ public class SlackConfig {
     @Value("${slack.webbhook-url}")
     private String SLACK_WEBHOOK_URL;
 
-    public static String DEFAULT_CHANNEL_ID = "C04282D342D";
+    public static String DEFAULT_CHANNEL_ID = "C04282D342D"; // TODO: env
 
     public static final int MAX_MESSAGE_SIZE = 10000;
 
