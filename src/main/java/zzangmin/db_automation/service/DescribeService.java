29b52d028e901955170e78d6059966785c7609d1
leezzangmin <ckdals12345678@gmail.com>
2024-04-15 18:31:02 +0900

    feat: schema blacklist 추가 (innodb)

--- src/main/java/zzangmin/db_automation/service/DescribeService.java
+++ src/main/java/zzangmin/db_automation/service/DescribeService.java
@@ -21,7 +21,7 @@ import java.util.stream.Collectors;
 @Service
 public class DescribeService {
 
-    public static final List<String> schemaBlackList = List.of("information_schema", "mysql", "performance_schema", "sys", "dba", "back_office", "test_schema");
+    public static final List<String> schemaBlackList = List.of("information_schema", "mysql", "performance_schema", "sys", "dba", "back_office", "test_schema", "innodb");
 
     private final AwsService awsService;
     private final MysqlClient mysqlClient;
