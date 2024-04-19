ea960f03a11e41ed307d66807c40f70df3311321
leezzangmin <ckdals12345678@gmail.com>
2024-04-19 20:46:12 +0900

    feat: 슬랙 요청 검증 클래스 메서드 구현

--- src/main/java/zzangmin/db_automation/security/SlackRequestSignatureVerifier.java
+++ src/main/java/zzangmin/db_automation/security/SlackRequestSignatureVerifier.java
@@ -1,7 +1,38 @@
 package zzangmin.db_automation.security;
 
-import org.springframework.stereotype.Component;
+import javax.crypto.Mac;
+import javax.crypto.spec.SecretKeySpec;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.codec.binary.Hex;
+import org.springframework.stereotype.Component;
+import zzangmin.db_automation.config.SlackConfig;
+
+@Slf4j
+@RequiredArgsConstructor
+@Component
 public class SlackRequestSignatureVerifier {
+
+    private final SlackConfig slackConfig;
+
+    public boolean validateRequest(String slackSignature, String timestamp, String requestBody) {
+        try {
+            String secret = slackConfig.slackAppSigningSecret;
+            String baseString = "v0:" + timestamp + ":" + requestBody;
+
+            SecretKeySpec secretKey = new SecretKeySpec(secret.getBytes(), "HmacSHA256");
+            Mac mac = Mac.getInstance("HmacSHA256");
+            mac.init(secretKey);
+
+            byte[] hash = mac.doFinal(baseString.getBytes());
+            String mySignature = "v0=" + Hex.encodeHexString(hash);
+
+            return mySignature.equals(slackSignature);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
 
 }
