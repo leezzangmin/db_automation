14600dae7a6024eed397d7b8506179f707272636
leezzangmin <ckdals12345678@gmail.com>
2024-04-19 20:15:21 +0900

    feat: RequestHistory 엔티티 클래스 생성

---
+++ src/main/java/zzangmin/db_automation/entity/RequestHistory.java
@@ -0,0 +1,29 @@
+package zzangmin.db_automation.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@ToString
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class RequestHistory {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private CommandType commandType;
+    private String command;
+    private String requestDoer;
+    private String databaseIdentifier;
+    private String schemaName;
+    private String tableName;
+    private LocalDateTime requestDatetime;
+    private LocalDateTime performDatetime;
+    private double executionDuration;
+}
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -25,10 +25,13 @@ CREATE TABLE IF NOT EXISTS back_office.schema_object (
 CREATE TABLE IF NOT EXISTS back_office.request_history (
     id bigint primary key auto_increment,
     request_doer varchar(64) not null comment '요청자 닉네임 ex) `august`',
-    request_type varchar(64) not null comment '요청 타입 ex) `add index`',
+    command_type varchar(64) not null comment '요청 타입 ex) `add index`',
     command text not null comment '요청 커맨드 ex) `create index...`',
+    database_identifier varchar(255) not null comment '대상 DB서버명',
+    schema_name varchar(255) not null comment '대상 스키마명',
+    table_name varchar(255) not null comment '대상 테이블명',
     request_datetime datetime not null comment '요청 시간',
     perform_datetime datetime not null comment '실제 수행 시간(시작시간)',
-    excution_duration double not null comment '소요 시간(초) ex) `12.403`(초)'
+    execution_duration double not null comment '소요 시간(초) ex) `12.403`(초)'
 
 ) comment '요청 내역(개발팀 DB요청 로그)';
