77d1b84c1c2ceffe7d4a1bf0cd3bf4cf52d107bc
leezzangmin <ckdals12345678@gmail.com>
2024-05-08 17:51:41 +0900

    add colum fix4567

--- src/main/java/zzangmin/db_automation/slackview/CreateIndexBlockPage.java
+++ src/main/java/zzangmin/db_automation/slackview/CreateIndexBlockPage.java
@@ -79,7 +79,7 @@ public class CreateIndexBlockPage {
     public List<LayoutBlock> handleAddColumn(List<LayoutBlock> currentBlocks, Map<String, Map<String, ViewState.Value>> values) {
         int lastInputColumnNameBlockIndex = findLastInputColumnNameBlockIndex(currentBlocks);
         int blockIdNumber = findBlockIdNumber(currentBlocks.get(lastInputColumnNameBlockIndex));
-        currentBlocks.add(lastInputColumnNameBlockIndex, BasicBlockFactory.findSinglelinePlainTextInput(SlackController.createIndexColumnNameTextInputId + (blockIdNumber + 1),
+        currentBlocks.add(lastInputColumnNameBlockIndex + 1, BasicBlockFactory.findSinglelinePlainTextInput(SlackController.createIndexColumnNameTextInputId + (blockIdNumber + 1),
                 inputIndexColumnNameLabel + (blockIdNumber + 1),
                 createIndexColumnPlaceHolder));
         return currentBlocks;
