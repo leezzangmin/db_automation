002735071f671d112fa5231d4ad8c648259b7777
leezzangmin <ckdals12345678@gmail.com>
2024-05-18 17:22:42 +0900

    test: @ActiveProfiles("test")  추가

--- src/test/java/zzangmin/db_automation/service/AwsServiceTest.java
+++ src/test/java/zzangmin/db_automation/service/AwsServiceTest.java
@@ -3,11 +3,13 @@ package zzangmin.db_automation.service;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class AwsServiceTest {
 
     @Autowired
