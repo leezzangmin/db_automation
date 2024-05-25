3a78f394426cca61840736bdf52a20852e5fee79
leezzangmin <ckdals12345678@gmail.com>
2024-05-25 17:52:43 +0900

    chore: Slack Verification Token 설정 추가

--- src/main/java/zzangmin/db_automation/config/SlackConfig.java
+++ src/main/java/zzangmin/db_automation/config/SlackConfig.java
@@ -15,14 +15,18 @@ public class SlackConfig {
     @Value("${slack.token}")
     public String slackToken;
     public static String slackAppSigningSecret;
+    public static String verificationToken;
+
     public static String DEFAULT_CHANNEL_ID;
     public static final int MAX_MESSAGE_SIZE = 10000;
 
     @Autowired
     public void setSlackAppSigningSecret(@Value("${slack.app-signing-secret}")String slackAppSigningSecret,
-                                         @Value("${slack.default-channel-id}")String DEFAULT_CHANNEL_ID) {
+                                         @Value("${slack.default-channel-id}")String DEFAULT_CHANNEL_ID,
+                                         @Value("${slack.verification-token}")String verificationToken) {
         this.slackAppSigningSecret = slackAppSigningSecret;
         this.DEFAULT_CHANNEL_ID = DEFAULT_CHANNEL_ID;
+        this.verificationToken = verificationToken;
     }
 
     @Bean
