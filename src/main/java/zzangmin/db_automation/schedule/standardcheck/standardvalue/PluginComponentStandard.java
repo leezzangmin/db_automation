a85ee0fcd5212bc9db98ecb7f2f5b75052e919b7
leezzangmin <ckdals12345678@gmail.com>
2024-06-19 22:03:48 +0900

    feat: plugin, component 표준 정의

---
+++ src/main/java/zzangmin/db_automation/schedule/standardcheck/standardvalue/PluginComponentStandard.java
@@ -0,0 +1,23 @@
+package zzangmin.db_automation.schedule.standardcheck.standardvalue;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class PluginComponentStandard {
+    public final static Map<String, String> pluginComponentStandardValues = new HashMap<>();
+
+    public final static List<String> essentialPluginComponents = List.of("validate_password");
+
+
+    static {
+        pluginComponentStandardValues.put("validate_password_check_user_name", "ON");
+        pluginComponentStandardValues.put("validate_password_dictionary_file", "");
+        pluginComponentStandardValues.put("validate_password_length", "8");
+        pluginComponentStandardValues.put("validate_password_mixed_case_count", "1");
+        pluginComponentStandardValues.put("validate_password_number_count", "1");
+        pluginComponentStandardValues.put("validate_password_policy", "MEDIUM");
+        pluginComponentStandardValues.put("validate_password_special_char_count", "1");
+    }
+
+}
