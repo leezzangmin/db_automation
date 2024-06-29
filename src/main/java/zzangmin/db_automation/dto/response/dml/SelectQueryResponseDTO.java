5c74c86ffea7b4c7e073714716ebf82dc3faf3d2
leezzangmin <ckdals12345678@gmail.com>
2024-06-29 15:11:26 +0900

    feat: ToString 추가

--- src/main/java/zzangmin/db_automation/dto/response/dml/SelectQueryResponseDTO.java
+++ src/main/java/zzangmin/db_automation/dto/response/dml/SelectQueryResponseDTO.java
@@ -2,7 +2,9 @@ package zzangmin.db_automation.dto.response.dml;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.ToString;
 
+@ToString
 @Getter
 @AllArgsConstructor
 public class SelectQueryResponseDTO {
