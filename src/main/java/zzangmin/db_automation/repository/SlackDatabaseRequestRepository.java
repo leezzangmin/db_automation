65b32f9ab6e43baa6f2f6eb8d8ea6a9f07469208
leezzangmin <ckdals12345678@gmail.com>
2024-11-26 16:59:13 +0900

    feat: findNotCompleted 조회 쿼리 메서드 추가

--- src/main/java/zzangmin/db_automation/repository/SlackDatabaseRequestRepository.java
+++ src/main/java/zzangmin/db_automation/repository/SlackDatabaseRequestRepository.java
@@ -1,13 +1,19 @@
 package zzangmin.db_automation.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import zzangmin.db_automation.entity.SlackDatabaseRequest;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface SlackDatabaseRequestRepository extends JpaRepository<SlackDatabaseRequest, Long> {
 
     Optional<SlackDatabaseRequest> findOneByRequestUUID(String requestUUID);
+
+    @Query("select s from SlackDatabaseRequest s where s.isComplete = false")
+    List<SlackDatabaseRequest> findNotCompleted();
+
 }
