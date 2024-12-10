7b40849df652cfec721b5882e3f9b27ae09c8401
leezzangmin <ckdals12345678@gmail.com>
2024-12-11 01:42:07 +0900

    fix: 오타 수정 postpix -> postfix

--- src/main/java/zzangmin/db_automation/standardvalue/SecretManagerStandard.java
+++ src/main/java/zzangmin/db_automation/standardvalue/SecretManagerStandard.java
@@ -2,10 +2,10 @@ package zzangmin.db_automation.standardvalue;
 
 public class SecretManagerStandard {
 
-    public static final String DB_CREDENTIAL_POSTPIX = "-db-credential";
+    public static final String DB_CREDENTIAL_POSTFIX = "-db-credential";
 
     public static String generateStandardSecretManagerName(String serviceNameTagValue, String currentProfile) {
-        String secretName = serviceNameTagValue + "-" + currentProfile + DB_CREDENTIAL_POSTPIX;
+        String secretName = serviceNameTagValue + "-" + currentProfile + DB_CREDENTIAL_POSTFIX;
         return secretName;
     }
 
